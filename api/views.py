205cf5648eb8c58086a3c8b7c8ca9571bf36ac6c
keshavarzatoosa.dev <hel_sok_tikay@yahoo.com>
2025-01-19 04:19:51 -0800

    add post method to BlogPostListView

--- api/views.py
+++ api/views.py
@@ -11,3 +11,10 @@ class BlogPostListView(APIView):
         blog_posts = BlogPost.objects.all()
         serializer = BlogPostSerializer(blog_posts, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = BlogPostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
