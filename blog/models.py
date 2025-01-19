a1fbd96c7989387bb607455c13cc784f46a35810
keshavarzatoosa.dev <hel_sok_tikay@yahoo.com>
2025-01-19 03:22:23 -0800

    create Tag and BlogPost models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class BlogPost(models.Model):
+
+    CHOICE_CATEGORY = [
+        ('T', 'Technology'),
+        ('S', 'Sport'),
+        ('N', 'News')
+    ]
+
+    title = models.CharField(max_length=250)
+    content = models.TextField()
+    category = models.CharField(choices=CHOICE_CATEGORY, max_length=1)
+    tags = models.ManyToManyField(Tag, related_name='blog_posts', blank=True)
+    create = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
