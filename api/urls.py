1237b141bab93220194e52cd96f5d7c0d2912a63
keshavarzatoosa.dev <hel_sok_tikay@yahoo.com>
2025-01-19 05:19:21 -0800

    add BlogPostDetailView class for get one blog-post

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import BlogPostListView
+from .views import BlogPostListView, BlogPostDetailView
 
 app_name = 'api'
 
 urlpatterns = [
     path('blog-posts/', BlogPostListView.as_view(), name='blog-post-list'),
+    path('blog-post/<int:pk>', BlogPostDetailView.as_view(), name='blog-post-detail'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 from rest_framework import status
 from blog.models import BlogPost
 from .serializers import BlogPostSerializer
@@ -18,3 +19,13 @@ class BlogPostListView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class BlogPostDetailView(APIView):
+
+    def get(self, request, pk):
+        blog_post = get_object_or_404(BlogPost, pk=pk)
+        serializer = BlogPostSerializer(blog_post)
+        return Response(serializer.data)
+
+
